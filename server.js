9dbc2eff0dabe057f1416fa00ea5e1c390344e23
svetlio <svetlio1994@gmail.com>
2017-07-14 22:11:02 +0300

    Add search by video name and showing of the results

--- server.js
+++ server.js
@@ -3,33 +3,49 @@ var spawn = require('child_process').spawn;
 var express = require('express');
 var app = express();
 var robot = require('robotjs');
+var request = require('request');
+
 
 var currentBrowserProc = null;
+var youtubeKey = 'AIzaSyAph2AtB2-kc2-bftUJkjG8ZHwkuiirZC8';
+
 
 app.use(express.static(__dirname));
+app.set('view engine', 'ejs');
 
 
 app.get('/', function (req, res) {
-    res.render('index.html');
+    res.render('index');
 });
+
+
+app.get('/search/', function (req, res) {
+    var searchQ = req.query.q;
+    var url = 'https://www.googleapis.com/youtube/v3/search?key=' + youtubeKey + '&part=snippet&' + 'q=' + searchQ;
+    request(url, function (e, r, body) {
+        res.send(body);
+    });
+});
+
 
 app.get('/yt-stream/:url', function (req, res) {
     if (currentBrowserProc) {
         currentBrowserProc.kill();
     }
 
-    res.send('Streaming YouTube Video...');
     currentBrowserProc = spawn('chromium-browser', [`https://www.youtube.com/watch?v=${req.params.url}`]);
     setTimeout(function () {
         robot.keyTap('f');
-    }, 5000);
+    }, 15000);
 });
+
 
 app.get('/kill/', function (req, res) {
     if (currentBrowserProc) {
         currentBrowserProc.kill();
     }
 });
+
 
 // Setup PiCAST Server
 var srv = app.listen(3000, function () {
@@ -38,5 +54,3 @@ var srv = app.listen(3000, function () {
 
         console.log('Access at http://%s:%s', host, port);
 });
-
-console.log('EVERYTHING IS UP')
